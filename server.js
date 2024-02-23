ecd8f6818ddb60d341b3cdc448945cc49e3591ce
Rahul <rahul.red1801@gmail.com>
2024-02-23 14:54:17 +0530

    update

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const rfs = require("rotating-file-stream");
 
-
-
 const connectDB = require("./db/connect");
 //Routes
 const authRouter = require("./routes/authRoute");
 const jobseekerRouter = require("./routes/jobSeekerRoute");
 const companyRouter = require("./routes/companyRoute");
 const adminRouter = require("./routes/adminRoute");
-
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -43,11 +40,24 @@ app.use(cookieParser(process.env.JWT_SECRET));
 //Inbuilt middleware
 app.use(express.static("./public"));
 
-
 app.use("/api/auth", authRouter);
 app.use("/api/jobseeker", jobseekerRouter);
 app.use("/api/company", companyRouter);
-app.use('/api/admin', adminRouter);
+app.use("/api/admin", adminRouter);
+
+//Error middleware
+app.use((err, req, res, next) => {
+  console.log("Error handling middleware called");
+  console.log("Path", req.path);
+  console.error("Error", err);
+
+  if (err.type === "redirect") {
+    res.redirect("/error");
+  } else if (err.type === "time-out") {
+    res.status(408).send(err);
+  } else res.status(500).send(err);
+  next();
+});
 
 const port = process.env.PORT || 8080;
 const start = async () => {
@@ -61,20 +71,5 @@ const start = async () => {
     console.log(error);
   }
 };
-
-//Error middleware
-router.use((err, req, res, next) => {
-  console.log("Error handling middleware called");
-  console.log("Path", req.path);
-  console.error("Error", err);
-
-  if (err.type === "redirect") {
-    res.redirect("/error");
-  } else if (err.type === "time-out") {
-    res.status(408).send(err);
-  } else res.status(500).send(err);
-  next();
-});
-
 
 start();
